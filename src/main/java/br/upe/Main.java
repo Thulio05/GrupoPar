2d6b8f7b4a13981ee3f0a8730113b5312a5b4b7e
Everton Rodrigues Silva <99847875+EvertnRS@users.noreply.github.com>
2024-06-18 12:25:23 -0300

    Update Main.java

--- src/main/java/br/upe/Main.java
+++ src/main/java/br/upe/Main.java
@@ -4,6 +4,6 @@ package br.upe;
 public class Main {
     public static void main( String[] args ) {
         String[] resultado = ResultadoMegaSena.obtemUltimoResultado();
-        for(String num: resultado) System.out.println(num);
+        for(String num: resultado) System.out.println("Números: " + num);
     }
 }
