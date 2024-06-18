d4d68b859c38b600ef4e6ed0ef5e152278a9483d
DeyvidMariano <159393860+DeyvidMariano@users.noreply.github.com>
2024-06-18 11:56:48 -0300

    Update Main.java

--- src/main/java/br/upe/Main.java
+++ src/main/java/br/upe/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main( String[] args ){
         String[] resultado = ResultadoMegasena.obtemUltimoResultado();
         for (String dezena: resultado) {
-            System.out.print(dezena + " ");
+            System.out.print(dezena + ",");
         }
     }
 }
