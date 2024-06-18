01b5f8ab41afce23e80f09fae4b3322f6675e580
Marcelo Spacca Barros Ferreiro <marceloferreiro05@gmail.com>
2024-06-18 12:08:37 -0300

    Update Main.java

--- src/main/java/br/upe/Main.java
+++ src/main/java/br/upe/Main.java
@@ -3,7 +3,6 @@ package br.upe;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-//hello, deyvid
 public class Main {
     public static void main( String[] args ){
         String[] resultado = ResultadoMegasena.obtemUltimoResultado();
