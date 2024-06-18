252a31eb3e0d5561ce90eb6e082e53ddf00fb668
kio <kaio.viana@upe.br>
2024-06-18 12:44:21 -0300

    Update Main.java

--- src/main/java/br/upe/Main.java
+++ src/main/java/br/upe/Main.java
@@ -3,7 +3,7 @@ package br.upe;
 
 public class Main {
     public static void main( String[] args ) {
-        String[] resultado = ResultadoMegaSena.obtemUltimoResultado();
+        String[] resultado = ResultadoMegasena.obtemUltimoResultado();
         for(String num: resultado) System.out.println(num);
     }
 }
