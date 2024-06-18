9b22b934aeae16d15f66ce7262474b7a4b23f10a
Victor Salgado <joaosalgado605@gmail.com>
2024-06-18 12:27:51 -0300

    Delete src directory

--- src/main/java/br/upe/Main.java
+++
@@ -1,9 +0,0 @@
-
-package br.upe;
-
-public class Main {
-    public static void main( String[] args ) {
-        String[] resultado = ResultadoMegaSena.obtemUltimoResultado();
-        for(String num: resultado) System.out.println("Números: " + num);
-    }
-}
