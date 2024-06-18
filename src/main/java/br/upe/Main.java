7b8ce58f29c9c3912aae66236cec2a72a56f0fb6
Marcelo Spacca Barros Ferreiro <marceloferreiro05@gmail.com>
2024-06-18 11:58:11 -0300

    Update Main.java

--- src/main/java/br/upe/Main.java
+++ src/main/java/br/upe/Main.java
@@ -3,11 +3,6 @@ package br.upe;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/**
- * Hello world!
- *
- */
-
 //hello, deyvid
 public class Main {
     public static void main( String[] args ){
